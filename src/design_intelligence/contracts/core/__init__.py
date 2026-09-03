ac1648dd52b7b96a6fe98374cc0a1783d92b24f7
gitzhangcd <kingdynsaty@gmail.com>
2026-09-03 18:24:22 +0800

    feat: export P1 hashing API

--- src/design_intelligence/contracts/core/__init__.py
+++ src/design_intelligence/contracts/core/__init__.py
@@ -1,6 +1,7 @@
 """Public P0-P1 core contract API."""
 
 from .base import DIModel, FrozenDIModel
+from .hashing import canonical_json_bytes, compute_content_hash
 from .refs import CanonicalRef, ContentHash, ExactObjectRef, LogicalObjectRef, ObjectRef
 from .types import CanonicalObjectClass, LogicalId, ObjectId, ObjectType, SchemaVersion
 
@@ -17,4 +18,6 @@ __all__ = [
     "ObjectRef",
     "ObjectType",
     "SchemaVersion",
+    "canonical_json_bytes",
+    "compute_content_hash",
 ]
