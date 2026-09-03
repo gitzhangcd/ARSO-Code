bb77ae5faae1c209bc98945ec98e0fa48ae86a56
gitzhangcd <kingdynsaty@gmail.com>
2026-09-03 18:21:00 +0800

    feat: export P1 exact reference contracts

--- src/design_intelligence/contracts/core/__init__.py
+++ src/design_intelligence/contracts/core/__init__.py
@@ -1,14 +1,20 @@
-"""Public P0 core contract API."""
+"""Public P0-P1 core contract API."""
 
 from .base import DIModel, FrozenDIModel
+from .refs import CanonicalRef, ContentHash, ExactObjectRef, LogicalObjectRef, ObjectRef
 from .types import CanonicalObjectClass, LogicalId, ObjectId, ObjectType, SchemaVersion
 
 __all__ = [
     "CanonicalObjectClass",
+    "CanonicalRef",
+    "ContentHash",
     "DIModel",
+    "ExactObjectRef",
     "FrozenDIModel",
     "LogicalId",
+    "LogicalObjectRef",
     "ObjectId",
+    "ObjectRef",
     "ObjectType",
     "SchemaVersion",
 ]
